52593c2556682ab07ee4acaf3e2406a036ec6a88
pratikbhongale <pratikbhongale01@gmail.com>
2026-03-20 10:40:22 +0530

    input.sh file

--- basic/input.sh
+++ basic/input.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 <<comment
 This script is about taking a input from user
+we take input using read
+we can also use -p to enter the input in the same line
 comment
 
 echo "What is ur name"
@@ -9,3 +11,12 @@ echo "hello, $name"
 
 read -p "What is your education? " ed
 echo "Congratulation on $ed degree"
+
+read -p "Enter a number: " num
+echo "You entered: $num"
+
+# Adding two numbers
+read -p "Enter First number: " num1
+read -p "Enter Second number: " num2
+sum=$((num1 + num2))
+echo "Sum is $sum"
