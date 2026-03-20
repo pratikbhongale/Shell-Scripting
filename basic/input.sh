d110fb42cbed2d37e105668b4707ee1d7547b547
pratikbhongale <pratikbhongale01@gmail.com>
2026-03-20 10:28:22 +0530

    input.sh

---
+++ basic/input.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+<<comment
+This script is about taking a input from user
+comment
+
+echo "What is ur name"
+read name
+echo "hello, $name"
+
+read -p "What is your education? " ed
+echo "Congratulation on $ed degree"
