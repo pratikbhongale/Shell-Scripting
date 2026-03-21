fce5b1ede7e086159da66dc9c4c7c9eca43012f3
pratikbhongale <pratikbhongale01@gmail.com>
2026-03-21 11:27:58 +0530

    array update

--- basic/array.sh
+++ basic/array.sh
@@ -6,4 +6,17 @@ echo ${my_array[3]}
 echo ${my_array[@]}
 echo ${#my_array[@]}
 # to display all the elements in an array @ is used
-# #to print total number of elements in the array # is defined at start 
+# #to print total number of elements in the array # is defined at start
+
+fruits=("apple" "banana" "mango")
+echo ${fruits[1]}
+echo ${fruits[0]}
+echo ${#fruits[@]}
+
+
+array=("A" "B" "C" "D" "E")
+echo "${array[@]}"
+echo "${array[@]:1:3}"
+
+text="ABCDE"
+echo "${text:3}"
