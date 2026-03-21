a1dda4319c37f2c3479ade1d7c796025a8a3b8fa
pratikbhongale <pratikbhongale01@gmail.com>
2026-03-21 12:35:26 +0530

    if-else update

--- basic/if-else.sh
+++ basic/if-else.sh
@@ -8,7 +8,7 @@ else
 	echo "Hello ${name}, Welcome to the Shell Scripting!!"
 fi
 comment
-
+<<comment
 num=10
 if [[ $num -gt  5 ]]
 then
@@ -23,4 +23,33 @@ then
 	echo "number is equal"
 else
 	echo "Number is not 4"
+fi
+
+num=10
+if [[ $num -gt 10 ]]
+then
+	echo "Number is greater than 10"
+elif [[ $num -eq 10 ]]
+then
+	echo "Equal to 10"
+else
+	echo "Number is less than 10"
+fi
+comment
+
+read -p "Enter the Number: " num
+if [ $num -gt 10 -a $num -lt 20 ]
+then
+	echo "The Number is between 10 and 20"
+else
+	echo "Outside Range"
+fi
+
+
+read -p "Enter the Number: " num1
+if [ $num1 -gt 10 -o $num1 -lt 20 ]
+then
+	echo "The Number is between 10 and 20"
+else
+	echo "Outside Range"
 fi
