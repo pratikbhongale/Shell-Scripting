dca19c0a2ca1a583ba38ba0fab1ef0386700ada2
pratikbhongale <pratikbhongale01@gmail.com>
2026-03-22 10:58:00 +0530

    root & service example

--- basic/real-world.sh
+++ basic/real-world.sh
@@ -11,3 +11,19 @@ else
 	sleep 5
 	echo "File is created successfully"
 fi
+read -p "Enter the service name to check the availability: " service
+status=$(systemctl is-active $service)
+if [ "$status" = "active" ]
+then
+	echo "Service is running"
+else
+	echo "Service is not running"
+fi
+
+user=$(whoami)
+if [ "$user" = "root" ]
+then
+	echo "You are root user"
+else
+	echo "You are not root user"
+fi
