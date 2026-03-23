18e6796e807209d93535b58c919600f23f1a343b
pratikbhongale <pratikbhongale01@gmail.com>
2026-03-23 10:54:42 +0530

    updated

--- basic/loops.sh
+++ basic/loops.sh
@@ -17,8 +17,28 @@ for i in 1 2 3 4 5
 do
 	echo "Number: $i"
 done
-
+#
 for i in {1..6}
 do
 	echo "$i"
 done
+#
+for i in {5..1}
+do
+	echo "$i"
+done
+
+#
+for server in server1 server2 server3
+do
+	echo "connecting to $server"
+#	sleep 2
+done
+
+
+for file in *.txt
+do
+	echo "Processing $file"
+done
+
+
