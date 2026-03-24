3710e9405ad0647e93f67713c87f27503f411b84
pratikbhongale <pratikbhongale01@gmail.com>
2026-03-24 09:20:29 +0530

    more examples

--- basic/until-loop.sh
+++ basic/until-loop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #while runs while the condition is true
 #until runs while condition is false
+<<comment
 i=1
 until [ $i -gt 5 ]
 do
@@ -24,3 +25,28 @@ do
 	read -p "Try again: " input
 done
 echo "Access granted"
+
+
+file="demo.txt"
+until [ -f "$file" ]
+do
+	echo "Waiting for the file $file......."
+	sleep 2
+done
+echo "$file found!"
+
+comment
+i=2
+until [ $i -gt 10 ]
+do
+	echo $i
+	i=$((i+2))
+done
+
+read -p "Enter a number > 10: " num
+until [ $num -gt 10 ]
+do
+	echo "Number is too small"
+	read -p "Try again: " num
+done
+echo "Good!"
