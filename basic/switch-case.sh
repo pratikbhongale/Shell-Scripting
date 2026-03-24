e46f6b374d398d1e0edee22c08e9cb7135f912f1
pratikbhongale <pratikbhongale01@gmail.com>
2026-03-24 10:03:04 +0530

    switch-case

--- basic/switch-case.sh
+++ basic/switch-case.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+<<example1
 read -p "Enter the name of car: " car
 case $car in
 	Tesla)
@@ -12,6 +13,42 @@ case $car in
 		;;
 	*)
 		echo -n "${car} is an unknown brand"
+		;;
+esac
+example1
+
+<<example2
+read -p "Enter the input: " input
+case $input in
+	[0-9])
+		echo "Single digit number"
+		;;
+	[a-z])
+		echo "Lowercase letter"
+		;;
+	[A-Z])
+		echo "Uppercase letter"
+		;;
+	*)
+		echo "Something Different"
+		;;
+esac
+example2
+
+
+read -p "Enter the environment: " env
+case $env in
+	dev | Dev | DEV)
+		echo "Development environment"
+		;;
+	test | Test | TEST)
+		echo "Test environment"
+		;;
+	prod | production)
+		echo "Production environment"
+		;;
+	*)
+		echo "Unknown environment"
 		;;
 esac
 
