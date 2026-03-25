95dc74112f2e1850180b850cf7d03e389baf0646
pratikbhongale <pratikbhongale01@gmail.com>
2026-03-25 12:11:27 +0530

    commit

---
+++ intermediate/array.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+<<comment
+servers=("app1" "app2" "db1")
+echo ${servers[0]}
+echo ${servers[1]} #to print specific element 
+echo ${servers[@]} #to print every element of the array
+echo ${#servers[@]} #to print length of the total element
+
+fruits=("apple" "banana" "cherry" "mango" "grapes")
+echo ${#fruits[@]}
+#for fruit in ${fruits} it willl only print the first element of the array
+for fruit in "${fruits[@]}" #to print all the elements of array
+do
+	echo "Fruit: $fruit"
+done
+comment
+servers=("app-1" "app-2" "db-1" "db-2")
+for server in "${servers[@]}"
+do
+	echo "Checking $server....."
+	sleep 2
+done	
