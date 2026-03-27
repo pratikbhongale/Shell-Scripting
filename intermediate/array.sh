ed9926a49b6c1ea9c227909ef82a8c1ad7e3c0e2
pratikbhongale <pratikbhongale01@gmail.com>
2026-03-27 19:17:08 +0530

    array late commit

--- intermediate/array.sh
+++ intermediate/array.sh
@@ -14,14 +14,13 @@ do
 	echo "Fruit: $fruit"
 done
 comment
-servers=("app-1" "app-2" "db-1" "db-2")
 <<comment
+servers=("app-1" "app-2" "db-1" "db-2")
 for server in "${servers[@]}"
 do
 	echo "Checking $server....."
 	sleep 2
 done	
-comment
 #to add element
 servers+=("API" "Cloud")
 echo ${servers[@]}
@@ -30,4 +29,47 @@ do
 	echo "checking $server....."
 	sleep 2
 done
+comment
 
+services=("nginx" "docker" "minikube")
+check="nginx"
+for service in "$services"
+do
+	if [ "$service" == "$check" ]; then
+		echo "service is already installed"
+	fi
+done
+
+<<comment
+#Reading array from user input
+read -p "Enter server name: " server
+for s in ${server[@]}
+do
+	echo "$server: $s"
+	echo "$s"
+done
+
+
+logs=("app.log" "data.log" "error.txt" "notes.txt")
+for file in ${logs[@]}
+do
+	if [[ $file == *.log ]]; then
+		echo "Log file: $file" 
+	fi
+done
+
+read -p "enter the array element: " arr
+echo "The array elements are ${arr[@]}"
+arr+=(4 5 6)
+echo "new element ${arr[@]}"
+echo "${!arr[@]}" #this prints the index value of the element present inside the array
+comment
+
+
+array=(1 2 3 4.3 true docker)
+echo ${array[@]}
+echo ${!array[@]}
+
+#to delete array indexes using unset builtin
+unset -v array[2]
+echo "${array[@]}"
