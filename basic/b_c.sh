bb61aca2b6bbe4f21fd63c6d36625b1c9b027013
pratikbhongale <pratikbhongale01@gmail.com>
2026-03-28 11:03:02 +0530

    update

--- basic/b_c.sh
+++ basic/b_c.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#continue is used to skip the current iteration and move to the next
+<<continue is used to skip the current iteration and move to the next
+-z is used to check if the variable or input has any empty value
+continue
 
 for i in 1 2 3 4 5 6 7
 do
@@ -9,4 +11,30 @@ do
 		continue
 	fi
 	echo "i is equal to $i"
+done
+
+
+for i in {1..10}
+do
+	if [ $i -eq 5 ]
+	then
+		continue 
+	else
+		echo $i
+	fi
+done
+
+#ignore empty inputs
+
+for name in "dev" "" "ops" "admin"
+do
+	if [ -z "$name" ]; then
+		continue
+		sleep 2
+		echo "Invalid input detected!!"
+		echo "skipping it.........."
+		sleep 2
+	fi
+	echo "Listing the environment provided: ${name}"
+	sleep 2
 done
