abf543610d410b8c62cb44d28dc3802e377f2843
pratikbhongale <pratikbhongale01@gmail.com>
2026-03-31 23:03:05 +0530

    adding devops focused example

--- intermediate/string.sh
+++ intermediate/string.sh
@@ -12,3 +12,16 @@ echo '$str2'
 
 text="DS"
 echo ${#text}
+
+#example 1
+read -p "Enter the environment to deploy: " env
+if [ "$env" == "prod" ]; then
+	echo "Deploying to production"
+else
+	echo "Deploying to pre prod"
+fi
+
+#example2
+tag="nginx-v1.2.3"
+version=${tag#nginx-v}
+echo $version
